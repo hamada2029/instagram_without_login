d55215f42daf27d3fee5b206379134989def709d
hamada2029 <8012459+hamada2029@users.noreply.github.com>
2020-04-22 16:05:32 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Instagram Without Login
+  
+You can open post and scroll window without login.  
+  
+## Usage
+1. Install Tampermonkey.
+    - [for Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+    - [for Firefox](https://addons.mozilla.org/ja/firefox/addon/tampermonkey/)
+    - [for Safari](https://www.tampermonkey.net/?browser=safari)
+1. Click [instagram_without_login.user.js](https://github.com/hamada2029/instagram_without_login/raw/master/instagram_without_login.user.js)
---
+++ instagram_without_login.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         instagram without login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.instagram.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    function setOverFlow(){
+        if(document.body.style.overflow == 'hidden'){
+            document.body.style.overflow = '';
+            console.log('remove overflow=hidden');
+        }
+
+        const links = document.querySelectorAll('a[href^="/p/"]');
+
+        links.forEach(
+            function(e){
+                if(e._flag){return;}
+                e.onclick = function(){
+                    open(e.href);
+                };
+                e._flag = true;
+            }
+        );
+
+
+
+
+    }
+
+    document.body.onmouseover = setOverFlow;
+
+    const _style = document.createElement('style');
+    _style.innerHTML = 'div[role="presentation"]{display:none}';
+    document.head.appendChild(_style);
+
+
+})();
+
