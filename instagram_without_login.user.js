ab7422c62611f1b649d14aeda43416a4e76955a0
hamada2029 <8012459+hamada2029@users.noreply.github.com>
2020-07-06 02:56:58 +0900

    fix "if"

--- instagram_without_login.user.js
+++ instagram_without_login.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         instagram without login
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.instagram.com/*
@@ -14,15 +14,6 @@
 
     // Your code here...
     function setOverFlow(){
-        if(! document.querySelector('[href*="/accounts/login"]')){
-            //console.log('logged in');
-            return;
-        }
-
-        if(document.URL.indexOf('/p/') > -1){
-            return;
-        }
-
         if(document.body.style.overflow == 'hidden'){
             document.body.style.overflow = '';
             console.log('remove overflow=hidden');
@@ -52,6 +43,15 @@
 
     }
 
+
+    if(! document.querySelector('[href*="/accounts/login"]')){
+        //console.log('logged in');
+        return;
+    }
+
+    if(document.URL.indexOf('/p/') > -1){
+        return;
+    }
     document.body.onmouseover = setOverFlow;
 
     const _style = document.createElement('style');
