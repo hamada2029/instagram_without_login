4fecdc65ae9fdabd98cbb6f27dd801aead47d371
hamada2029 <8012459+hamada2029@users.noreply.github.com>
2020-07-15 16:32:58 +0900

    by observer

--- instagram_without_login.user.js
+++ instagram_without_login.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         instagram without login
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.instagram.com/*
@@ -14,6 +14,16 @@
 
     // Your code here...
     function setOverFlow(){
+        if(! document.querySelector('[href*="/accounts/login"]')){
+            console.log('can not found login link');
+            return;
+        }
+
+        if(document.URL.indexOf('/p/') > -1){
+            console.log('post page');
+            return;
+        }
+
         if(document.body.style.overflow == 'hidden'){
             document.body.style.overflow = '';
             console.log('remove overflow=hidden');
@@ -44,21 +54,32 @@
     }
 
 
-    if(! document.querySelector('[href*="/accounts/login"]')){
-        //console.log('logged in');
-        return;
-    }
-
-    if(document.URL.indexOf('/p/') > -1){
-        return;
-    }
-    document.body.onmouseover = setOverFlow;
-
     const _style = document.createElement('style');
     _style.innerHTML = 'body > div[role="presentation"]{display:none}';
     document.head.appendChild(_style);
 
+    const target = document.querySelector('body');
+
+    // オブザーバインスタンスを作成
+    const observer = new MutationObserver(
+        function(mutations){
+            mutations.forEach(setOverFlow);
+        }
+    );
+
+    // オブザーバの設定
+    const config = {
+        childList: true,
+        subtree: true
+    };
+
+    // 対象ノードとオブザーバの設定を渡す
+    observer.observe(target, config);
+
+    setOverFlow(); // 1回実行しとく
+
 
 })();
+
 
 
